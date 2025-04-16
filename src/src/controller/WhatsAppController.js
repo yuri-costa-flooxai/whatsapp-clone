b2c4edcb2126cfa5bcad52d11bfd318126d9d6b0
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-16 13:18:13 -0300

    feat: Prototypes

--- src/src/controller/WhatsAppController.js
+++ src/src/controller/WhatsAppController.js
@@ -2,6 +2,7 @@ class WhatsAppController {
     constructor() {
         this.whatsapp = new WhatsApp();
         this.loadElements();
+        this.elementsPrototype();
     }
 
     loadElements() {
@@ -10,6 +11,66 @@ class WhatsAppController {
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element;
         });
+    }
+    
+    elementsPrototype() {
+        Element.prototype.hide = function() {
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function() {
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function() {
+            this.style.display = this.style.display === 'none' ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function(event, callback) {
+            this.addEventListener(event, callback);
+            return this;
+        }
+
+        Element.prototype.off = function(event, fn) {
+            EventSource.split(' ').forEach(e => {
+                this.removeEventListener(e, fn);
+            });
+            return this;
+        }
+
+        Element.prototype.css = function(styles) {
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+
+        Element.prototype.addClass = function(className) {
+            this.classList.add(className);
+            return this;
+        }
+
+        Element.prototype.removeClass = function(className) {
+            this.classList.remove(className);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(className) {
+            this.classList.toggle(className);
+            return this;
+        }
+
+        Element.prototype.hasClass = function(className) {
+            return this.classList.contains(className);
+        }
+
+        Element.prototype.html = function(html) {
+            this.innerHTML = html;
+            return this;
+        }
     }
 }
 
