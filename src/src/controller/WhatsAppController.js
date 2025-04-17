297cae003e54256b7d600d23fd0b5c1f3cb3cb74
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-17 11:18:59 -0300

    feat: menu events

--- src/src/controller/WhatsAppController.js
+++ src/src/controller/WhatsAppController.js
@@ -114,6 +114,7 @@ class WhatsAppController {
         });
 
         this.el.inputNameEditProfile.on('keypress', (e) => {
+
             if (e.key === 'Enter') {
 
                 e.preventDefault();
@@ -122,16 +123,80 @@ class WhatsAppController {
         });
 
         this.el.btnSavePanelEditProfile.on('click', () => {
-            console.log(this.el.inputNameEditProfile);
+
+            console.log(this.el.inputNameEditProfile.innerHTML);
+        });
+        
+        this.el.formPanelAddContact.on('submit', (e) => {
+
+            e.preventDefault();
+            let formData = new FormData(this.el.formPanelAddContact);
+        });
+        
+        HTMLFormElement.prototype.getForm = () => {
+
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function() {
+
+            let json = {};
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            });
+            return json;
+        }
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(contact => {
+
+            contact.on('click', () => {
+
+                this.el.homePanel.hide();
+                this.el.contactsMessagesListSelected.innerHTML = contact.querySelector('.contact-name').innerHTML;
+            });
+        });
+
+        this.el.btnAttach.on('click', () => {
+
+            e.stopPropagation();
+            this.el.inputAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+        });
+
+        this.el.btnAttachPhoto.on('click', () => {
+
+            this.el.inputAttachPhoto.click();
+        });
+
+        this.el.inputAttachCamera.on('change', () => {
+
+            this.el.inputAttachCamera.click();
+        });
+
+        this.el.inputAttachDocument.on('change', () => {
+
+            this.el.inputAttachDocument.click();
+        });
+
+        this.el.inputAttachContact.on('change', () => {
+
+            this.el.inputAttachContact.click();
         });
         
         
-        
-        
 
+            
+    }
+
+    closeMenuAttach(e) {
+
+        document.removeEventListener('click', this.closeMenuAttach.bind(this));
+        this.el.menuAttach.removeClass('open');
+        console.log('remove menu');
     }
 
     closeAllLeftPanel() {
+
         this.el.panelEditProfile.hide();
         this.el.panelAddContact.hide();
     }
